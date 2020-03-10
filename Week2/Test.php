6617db6964bae80b696017b88adec915fe7fcd87
Linh Nguyen <qlinh120198@gmail.com>
2020-03-10 10:33:50 +0700

    NguyenQuangLinh - 20162439

--- Week2/Test.php
+++ Week2/Test.php
@@ -1,6 +1,9 @@
-<?php
-$inquote = "Now Is The Time";
-$lower = strtolower($inquote);
-$upper = strtoupper($inquote);
-print ("upper=$upper lower=$lower");
-?>
+<html>
+    <head><title> Receiving Input </title> </head>
+    <body>
+        <font size=5>Thank You: Got Your Input.</font>
+        <?php
+        print ("<br>Your email address is $email");
+
+        print ("<br> Contact preference is $contact");
+        ?>
---
+++ Week2/basicform.php
@@ -0,0 +1,13 @@
+<html>
+    <head> <title>A Simple Form </title> </head>
+    <body>
+        <form action="hello.php" method="post">
+           Click submit
+           <br>
+           <input type ="text" name="Name">
+           <br>
+           <input type ="submit" value ="Click to submit">
+           <input type ="reset" value ="Erase and Restart">
+        </form>
+    </body> 
+</html>
