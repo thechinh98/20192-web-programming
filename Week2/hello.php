caddf464e336925649240130f84d1aeeb88f8157
Linh Nguyen <qlinh120198@gmail.com>
2020-03-10 10:43:48 +0700

    NguyenQuangLinh - 20162439

---
+++ Week2/hello.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <font size=5>Thank You: Got Your Input.</font>
+        <?php
+        $Name = $_POST["Name"];
+        print ("You have input $Name");
+        ?>
+</html>
